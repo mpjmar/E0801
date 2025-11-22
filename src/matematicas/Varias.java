ae7bc76e8c2a03d168afcf7d80d83cbccc5dcbb0
m. paz jimenez <meripeich@gmail.com>
2025-11-22 18:30:32 +0100

    terminado y probado

--- src/matematicas/Varias.java
+++ src/matematicas/Varias.java
@@ -1,89 +1,76 @@
 package matematicas;
 
-/**
- * Funciones matemáticas de proósito general
- */
 public class Varias {
 
 	/**
-	 * Devuelve verdadero si el número que se le pasa como parámetro 
-	 * es capicúa y falso en caso contrario.
+	 * Comprueba si un número pasado como parámetro es capicúa.
 	 * <p>
-	 * Un número capicúa es el que se lee igual de izquierda a derecha
-	 * que de derecha a izquierda.
+	 * Devuelve verdadero si el número que se pasa 
+	 * como parámetro es capicúa y falso en caso contrario.
 	 * 
-	 * @param num número del que se quiere saber si es capicúa.
-	 * @return verdadero si el número que se pasa como parámetro es
-	 * 		capicúa y falso en caso contrario.
+	 * @param num un número entero.
+	 * @return true si {@code num} es capicúa o false si no lo es.
 	 */
 	public static boolean esCapicua(long num) {
 		return num == voltea(num);
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #esCapicua(long)} que acepta un
-	 * {@code int}.
+	 * Versión sobrecargada de {@link #esCapicua(long)} que
+	 * admite un {@code int} como parámetro.
 	 * 
-	 * @param num número del que se quiere saber si es capicúa.
-	 * @return verdadero si el número que se pasa como parámetro es
-	 * 		capicúa y falso en caso contrario.
+	 * @param num un número entero.
+	 * @return true si {@code num} es capicúa o false si no no es.
 	 */
 	public static boolean esCapicua(int num) {
 		return esCapicua((long) num);
 	}
 
 	/**
-	 * Devuelve verdadero si el número que se pasa como parámetro 
-	 * es primo y falso en caso contrario.
-	 * <p>
-	 * Un número primo es un número natural mayor que 1 y divisible
-	 * únicamente entre el mismo y entre 1.
+	 * Devuelve el menor primo que es mayor al número que 
+	 * se pasa como parámetro.
 	 * 
-	 * @param num número del que se quiere saber si es primo.
-	 * @return verdadero si el número que se le pasa como parámetro es
-	 * 		primo y falso en caso contrario.
+	 * @param num un número entero.
+	 * @return el siguiente número primo mayor a {@code num}.
+	 */
+	public static int siguientePrimo(int num) {
+		while (!esPrimo(++num)) {}
+			return num;
+	}
+
+	/**
+	 * Comprueba si un número pasado como parámetro es primo.
+	 * <p>
+	 * Devuelve verdadero si el número que se pasa como parámetro 
+	 * es primo y falso de lo contrario.
+	 * 
+	 * @param num un número entero.
+	 * @return true si {@code num} es primo y false si no lo es.
 	 */
 	public static boolean esPrimo(long num) {
 		if (num < 2)
 			return false;
-		else {
-			for (long i = num / 2; i >= 2; i--)
-				if (num % i == 0)
-					return false;
-		}
+		for (long i = num / 2; i > 2; i--)
+			if (num % i == 0)
+				return false;
 		return true;
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #esPrimo(long)} que acepta un
-	 * {@code int}.
-	 * 
-	 * @param num número del que se quiere saber si es primo.
-	 * @return verdadero si el número que se pasa como parámetro es
-	 * 		primo y falso en caso contrario.
+	 * Versión sobrecargada de {@link #esPrimo(num)} que 
+	 * admite un {@code int}.
 	 */
 	public static boolean esPrimo(int num) {
 		return esPrimo((long) num);
 	}
 
 	/**
-	 * Devuelve el menor primo que es mayor al número que se pasa como
-	 * parámetro.
+	 * Dada una base y un exponente devuelve la potencia.
 	 * 
-	 * @param num un número entero.
-	 * @return el menor primo que es mayor al número pasado como parámetro.
-	 */
-	public static int siguientePrimo(int num) {
-		while (!esPrimo(++num)) {}
-		return num;
-	}
-
-	/**
-	 * Dada una base y un exponente, devuelve la potencia.
-	 * 
-	 * @param base base de la potencia.
-	 * @param exp exponente de la potencia.
-	 * @return número resultante de elevar la base a la potencia dada.
+	 * @param base un número entero.
+	 * @param exp el exponente al que queremos elevar el número.
+	 * @return el número {@code num} multiplicado por si mismo 
+	 * 		{@code exp} veces.
 	 */
 	public static double potencia(int base, int exp) {
 		if (exp == 0)
@@ -92,52 +79,50 @@ public class Varias {
 			return 1 / potencia(base, -exp);
 		int res = 1;
 		for (int i = 0; i < Math.abs(exp); i++)
-			res = res * base;
+			res *= base;
 		return res;
 	}
 
 	/**
-	 * Cuenta el número de dígitos de un número entero.
+	 * Cuenta el número de dígitos de un número entero 
+	 * pasado por parámetro.
 	 * 
-	 * @param num número al que se le quieren contar los dígitos.
-	 * @return número de dígitos del número que se pasa como parámetro.
+	 * @param num un número entero.
+	 * @return el número de dígitos de {@code num}.
 	 */
 	public static int digitos(long num) {
-		if (num < 0)
-			num = -num;
 		if (num == 0)
 			return 1;
-		else {
-			int dig = 0;
-			while (num > 0) {
-				num /= 10;
-				dig++;
-			}
-			return dig;
+		if (num < 1)
+			num = -num;
+		int n = 0;
+		while (num > 0){
+			num /= 10;
+			n++;
 		}
+		return n;
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #digitos(long)} que acepta un
-	 * {@code int}.
-	 * 
-	 * @param num número al que se le quieren contar los dígitos.
-	 * @return número de dígitos del número que se pasa como parámetro.
+	 * Versión sobrecargada de {@link #digitos(double)} que 
+	 * admite un {@code int}.
+	 * @param num un número entero.
+	 * @return el número de dígitos de {@code num}.
 	 */
 	public static int digitos(int num) {
 		return digitos((long) num);
 	}
 
 	/**
-	 * Le da la vuelta a un número.
+	 * Le da la vuelta a un número entero pasado por parámetro.
 	 * 
-	 * @param num número al que se le quiere dar la vuelta.
-	 * @return número volteado (al revés).
+	 * @param num un número entero.
+	 * @return el número {@code num} invertido.
 	 */
 	public static long voltea(long num) {
-		long volteado = 0;
 		if (num < 0)
 			return -voltea(-num);
+		long volteado = 0;
 		while (num > 0) {
 			volteado = volteado * 10 + num % 10;
 			num /= 10;
@@ -146,229 +131,229 @@ public class Varias {
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #voltea(long)} que acepta un
-	 * {@code int}.
+	 * Versión sobrecargada de {@link #voltea(long)} que
+	 * admite un {@code int}.
 	 * 
-	 * @param num número al que se le quiere dar la vuelta.
-	 * @return número volteado (al revés).
+	 * @param num un número entero.
+	 * @return el número {@code num} invertido.
 	 */
 	public static int voltea(int num) {
 		return (int) voltea((long) num);
 	}
 
 	/**
-	 * Devuelve el dígito que está en la posiciín n-ésima de un número.
+	 * Devuelve el dígito que está en la posición n de un número 
+	 * entero. 
+	 * <p>
+	 * Se empieza contando por el 0 de izquierda a derecha.
 	 * 
-	 * @param num número entero.
-	 * @param pos posición dentro del número {@code num}.
-	 * @return dígito que está en la posicion {@code pos} del número
-	 * 		{@code num} empezando a contar por la izquierda.
+	 * @param num un número entero.
+	 * @param pos la posición en la que se busca el dígito.
+	 * @return el dígito que se encuentra en la posición {@code pos}
+	 * 		dentro del número {@code num}.
 	 */
 	public static int digitoN(long num, int pos) {
-		num = voltea(num);
-		while (pos-- > 0)
-			num /= 10;
-		return (int)(num % 10);
+		num = Math.abs(num);
+		num /= potencia(10, digitos(num) - pos - 1);
+		return (int) num % 10;
 	}
 
 	/**
-	 * Versión sobrecargada del {@link #digitoN(long, int)} que acepta un
-	 * {@code int}.
+	 * Versión sobrecargada de {@link #digitoN(long, pos)} que
+	 * admite un {@cod int}.
 	 * 
-	 * @param num número entero.
-	 * @param pos posición dentro del número {@code num}.
-	 * @return dígito que está en la posición {@code pos} del número
-	 * 		{@code num} empezando a contar por la izquierda.
+	 * @param num un número entero.
+	 * @param pos la posición en la que se busca el dígito.
+	 * @return el dígito que se encuentra en la posición {@code pos}
+	 * 		dentro del número {@code num}.
 	 */
-	public static int digitoN(int num, int pos) {
-		return digitoN((long) num, pos);
+	public static int digitoN(int num, int dig) {
+		return digitoN((long) num, dig);
 	}
 
 	/**
-	 * Da la posición de la primera ocurrencia de un dígito dentro de un
-	 * número entero. Si no se encuentra, devuelve -1.
+	 * Da la posición de la primera ocurrencia de un dígito dentro
+	 * de un número entero. Si no lo encuentra, devuelve -1.
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito a buscar dentro del número.
-	 * @return posición de la primera ocurrencia del dígito dentro del
-	 * 		número o -1 si no se encuentra.
+	 * @param num un número entero.
+	 * @param dig el dígito que se busca.
+	 * @return la posición de {@code dig} dentro de {@code num} o
+	 * 		-1 si no se encuentra.
 	 */
 	public static int posicionDeDigito(long num, int dig) {
 		int pos;
-		for (pos = 0; (pos < digitos(num)) && (digitoN(num, pos) != dig); pos++) {}
-		if (pos == digitos(num))
+		for (pos = 0; pos < digitos(num) && digitoN(num, pos) != dig; pos++) {}
+		if (pos == digitos(num)) 
 			return -1;
-		else
-			return pos;
+		else return 
+			pos;
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #posicionDeDigito(long, int)} que
-	 * acepta un {@code int}.
+	 * Versión sobrecargada de {@link #posicionDeDigito(long, dig)}
+	 * que admite un {@code int}.
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito a buscar dentro del número.
-	 * @return posición de la primera ocurrencia del dígito dentro del
-	 * 		número o -1 si no se encuentra.
+	 * @param num un número entero.
+	 * @param dig el dígito que se busca.
+	 * @return la posición de {@code dig} dentro de {@code num} o
+	 * 		-1 si no se encuentra.
 	 */
-	public static int posicionDeDigito(int num, int dig) {
-		return posicionDeDigito((long) num, dig);
+	public static int posicionDeDigito(int num, int pos) {
+		return posicionDeDigito((long) num, pos);
 	}
 
 	/**
-	 * Le quita a un número {@code n} dígitos por la derecha.
+	 * Le quita a un número entero n dígitos por detrás (por la derecha).
 	 * 
-	 * @param num número entero.
-	 * @param n número de dígitos que se le van a quitar.
-	 * @return número inicial {@code num} con {@code n} dígitos menos
-	 * 		quitados de la derecha.
+	 * @param num un número entero.
+	 * @param n el número de dígitos a eliminar.
+	 * @return el número {@code num} sin los {@code n} dígitos de 
+	 * su derecha.
 	 */
 	public static long quitaPorDetras(long num, int n) {
 		return num / (long) potencia(10, n);
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #quitaPorDetras(long, int)} que
-	 * acepta un {@code int}.
+	 * Versión sobrecargada de {@link #quitaPorDetras(long, int)} 
+	 * que admite un {@code int}.
 	 * 
-	 * @param num número entero.
-	 * @param n número de dígitos que se le van a quitar.
-	 * @return número inicial {@code num} con {@code n} dígitos menos
-	 * 		quitados de la derecha.
+	 * @param num un número entero.
+	 * @param n el número de dígitos a eliminar.
+	 * @return el número {@code num} sin los {@code n} dígitos de 
+	 * su derecha.
 	 */
 	public static int quitaPorDetras(int num, int n) {
 		return (int) quitaPorDetras((long) num, n);
 	}
 
 	/**
-	 * Le quita a un número {@code n} dígitos por la derecha.
+	 * Le quita a un número entero n dígitos por delante (por la izquierda).
 	 * 
-	 * @param num número entero.
-	 * @param n número de dígitos que se le van a quitar.
-	 * @return número inicial {@code num} con {@code n} dígitos menos
-	 * 		quitados de la izquierda.
+	 * @param num un número entero.
+	 * @param n el número de dígitos a eliminar.
+	 * @return el número {@code num} sin los {@code n} dígitos de 
+	 * su izquierda.
 	 */
 	public static long quitaPorDelante(long num, int n) {
-		num = pegaPorDetras(num, 1);
-		num = voltea(quitaPorDetras(voltea(num), n));
-		num = quitaPorDetras(num, 1);
-		return num;
+		if (n > digitos(num))
+			return 0;
+		return num % (long) potencia(10, digitos(num) - n);
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #quitaPorDelante(long, int)} que
-	 * acepta un {@cod int}.
+	 * Versión sobrecargada de {@link #quitaPorDelante(long, int)} 
+	 * que admite un {@code int}.
 	 * 
-	 * @param num número entero.
-	 * @param dig número de dígitos que se le van a quitar.
-	 * @return número inicial {@code num} con {@code n} dígitos menos
+	 * @param num un número entero.
+	 * @param n el número de dígitos a eliminar.
+	 * @return el número {@code num} sin los {@code n} dígitos de 
+	 * su izquierda.
 	 */
-	public static long quitaPorDelante(int num, int n) {
+	public static int quitaPorDelante(int num, int n) {
 		return (int) quitaPorDelante((long) num, n);
 	}
 
 	/**
-	 * Añade un dígito a un número por detrás (por la derecha).
+	 * Añade un dígito a un número entero por detrás (por la derecha).
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito que se le va a pegar por la derecha.
-	 * @return número inicial {@code num} con el dígito {@code dig}
-	 * 		pegado por la derecha.
+	 * @param num un número entero.
+	 * @param dig el dígito a añadir.
+	 * @return el número {@code num} con el {@code dig} dígito añadido 
+	 * por su derecha.
 	 */
 	public static long pegaPorDetras(long num, int dig) {
 		return juntaNumeros(num, dig);
 	}
 
 	/**
-	 * Versión sobrecargada de pegaPorDetras(long, int) que
-	 * acepta un {@code int}.
+	 * Versión sobrecargada de {@link #pegaPorDetras(long, int)} 
+	 * que admite un {@code int}.
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito que se le va a pegar por la derecha.
-	 * @return número inicial {@code num} con el dígito {@code dig}
-	 * 		pegado por la derecha.
+	 * @param num un número entero.
+	 * @param dig el dígito a añadir.
+	 * @return el número {@code num} con el {@code dig} dígito añadido
+	 * por su derecha.
 	 */
 	public static int pegaPorDetras(int num, int dig) {
 		return (int) pegaPorDetras((long) num, dig);
 	}
 
 	/**
-	 * Añade un dítigo a un número por delante (por la izquierda).
+	 * Añade un dígito a un número entero por delante (por la izquierda).
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito que se le va a pegar por la izquierda.
-	 * @return número inicial {@code num} con el dígito {@code dig}
-	 * 		pegado por la izquierda.
+	 * @param num un número entero.
+	 * @param dig el dígito a añadir.
+	 * @return el número {@code num} con el {@code dig} dígito añadido 
+	 * por su izquierda.
 	 */
 	public static long pegaPorDelante(long num, int dig) {
 		return juntaNumeros(dig, num);
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #pegaPorDelante(long, int)} que
-	 * acepta un {@code int}.
+	 * Versión sobrecargada de {@link #pegaPorDelante(long, int)} 
+	 * que admite un {@code int}.
 	 * 
-	 * @param num número entero.
-	 * @param dig dígito que se le va a pegar por la izquierda.
-	 * @return número inicial {@code num} con el dígito {@code dig}
-	 * 		pegado por la izquierda.
+	 * @param num un número entero.
+	 * @param dig el dígito a añadir.
+	 * @return el número {@code num} con el {@code dig} dígito añadido
+	 * por su izquierda.
 	 */
 	public static int pegaPorDelante(int num, int dig) {
 		return (int) pegaPorDelante((long) num, dig);
 	}
-	
+
 	/**
-	 * Toma como parámetros las posiciones inicial y final dentro de un
-	 * número y devuelve el trozo correspondiente.
-	 * <p>
-	 * Las posiciones se cuentan de izquierda a derecha comenzando por
-	 * el cero.
+	 * Toma como parámetros las posiciones incial y final dentro de un
+	 * número entero y devuelve el trozo correspondiente.
 	 * 
-	 * @param num número entero.
-	 * @param posInicial posición incial.
-	 * @param posFinal posición final.
-	 * @return trozo de número compuesto por los dígitos que van desde
-	 * 		la posición inicial hasta la posición final incluyendo ambos.
+	 * @param num un número entero.
+	 * @param posInic la posición incial.
+	 * @param posFin la posicióin final.
+	 * @return la porción del número {@code num} que se encuentra entre
+	 * la posición {@code posInic} y la posición {@code posFin} incluidos
+	 * ambos y comenzando en 0.
 	 */
-	public static long trozoDeNumero(long num, int posInicial, int posFinal) {
+	public static long trozoDeNumero(long num, int posInic, int posFin) {
 		int len = digitos(num);
-		num = quitaPorDelante(num, posInicial);
-		num = quitaPorDetras(num, len - posFinal - 1);
-		return num;
+		num = quitaPorDelante(num, posInic);
+		return quitaPorDetras(num, len - posFin - 1);
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #trozoDeNumero(long, int, int)} que
-	 * acepta un {@code int}.
+	 * Versión sobrecargada de {@link #trozoDeNumero(long, int, int)}
+	 * que admite un int.
 	 * 
-	 * @param num número entero.
-	 * @param posInicial posición incial.
-	 * @param posFinal posición final.
-	 * @return trozo de número compuesto por los dígitos que van desde
-	 * 		la psoición inicial hasta la posición final incluyendo ambos.
+	 * @param num un número entero.
+	 * @param posInic la posición incial.
+	 * @param posFin la posicióin final.
+	 * @return la porción del número {@code num} que se encuentra entre
+	 * la posición {@code posInic} y la posición {@code posFin} incluidos
+	 * ambos y comenzando en 0.
 	 */
-	public static int trozoDeNumero(int num, int posInicial, int posFinal) {
-		return (int) trozoDeNumero((long) num, posInicial, posFinal);
+	public static int trozoDeNumero(int num, int posInic, int posFin) {
+		return (int) trozoDeNumero((long) num, posInic, posFin);
 	}
 
 	/**
-	 * Pega dos números para formar uno solo.
+	 * Pega dos números enteros para formar uno.
 	 * 
-	 * @param num1 trozo que se pegará por la izquierda.
-	 * @param num2 trozo que se pegará por la derecha.
-	 * @return número compuesto de los trozos {@code num1} y {@code num2}.
+	 * @param num1 el número que se pegará por la izquierda.
+	 * @param num2 el número que se pegará por la derecha.
+	 * @return el número resultante de pegar {@code num1} y {@code num2}.
 	 */
 	public static long juntaNumeros(long num1, long num2) {
 		return num1 * (long) potencia(10, digitos(num2)) + num2;
 	}
 
 	/**
-	 * Versión sobrecargada de {@link #juntanúmeros(long, long)} que
-	 * acepta dos {@code int}.
+	 * Versión sobrecargada de {@link #juntaNumeros(double, double)}
+	 * que admite dos {@code int}.
 	 * 
-	 * @param num1 trozo que se pegará por la izquierda.
-	 * @param num2 trozo que se pegará por la derecha.
-	 * @return número compuesto de los trozos {@code num1} y {@code num2}.
+	 * @param num1 el número que se pegará por la izquierda.
+	 * @param num2 el número que se pegará por la derecha.
+	 * @return el número resultante de pegar {@code num1} y {@code num2}.
 	 */
 	public static int juntaNumeros(int num1, int num2) {
 		return (int) juntaNumeros((long) num1, (long) num2);
